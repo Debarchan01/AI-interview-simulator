2ecf9ffccaf9b6aaaf9b7100eea18935da0227b3
Debarchan Parida <debarchan132002@gmail.com>
2025-07-19 10:35:46 +0530

    new modifications

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,12 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from './schema'
-const sql = neon(process.env.DRIZZLE_DB_URL);
-export const db = drizzle(sql,{schema} );
+import * as schema from './schema';
+
+if (!process.env.NEXT_PUBLIC_DRIZZLE_DB_URL) {
+  throw new Error("🚨 DRIZZLE_DB_URL is not set. Check your .env.local file.");
+}
+
+const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
+export const db = drizzle(sql, { schema });
+
+
